e3751521ed9c9841fe0668795a4c253849c46dab
tonysodeep <hynguyen2727@gmail.com>
2022-03-19 13:29:39 +0700

    add sample test file to success deloyment

--- app.test.js
+++ app.test.js
@@ -1,11 +1,12 @@
-const app = require("./app")
-const supertest = require("supertest")
-const request = supertest(app)
+const appTesting = require('./app-test');
+const supertest = require('supertest');
+const request = supertest(appTesting);
 
-describe("/test endpoint", () => {
-    it("should return a response", async () => {
-        const response = await request.get("/test")
-        expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
-    })
-})
+describe('/test end point', () => {
+  it('should return a response', async () => {
+    const response = await request.get('/test');
+    console.log(response);
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Hello world');
+  });
+});
